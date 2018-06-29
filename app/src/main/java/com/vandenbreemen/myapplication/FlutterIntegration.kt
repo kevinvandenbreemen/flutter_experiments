1f075e1fd18ad7ee94f8bf671001f3746e5a1d78
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-06-29 10:10:48 -0400

    KEV0-0:  Initial prototype for including Flutter in Android

---
+++ app/src/main/java/com/vandenbreemen/myapplication/FlutterIntegration.kt
@@ -0,0 +1,12 @@
+package com.vandenbreemen.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FlutterIntegration : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flutter_integration)
+    }
+}
