38ea98371e1d9b99b5206bcb0c26d8804c96c1fe
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-06-29 15:12:38 -0400

    KEV0-0:  Spruce up appearance

--- flutter_module/lib/business_logic.dart
+++ flutter_module/lib/business_logic.dart
@@ -36,7 +36,7 @@ class _GalleryChooserState extends State<GalleryChooser> {
 
   }
 
-  void onSelectUrl(String url){
+  void _playVideo(GalleryItem item){
 
   }
 
@@ -51,30 +51,48 @@ class _GalleryChooserState extends State<GalleryChooser> {
               child: new ListView.builder(
               itemBuilder: (context, index){
                 GalleryItem item = imageUrls[index];
-                return new Container(
-                  height: 300.0,
-                  width: 300.0,
-                  margin: new EdgeInsets.symmetric(
-                    vertical: 2.0,
-                    horizontal: 2.0
-                  ),
-                  child: new Column(
-                    children: <Widget>[
-                      Expanded(child:Image.network(item.url)),
-                      new Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                return new Stack(
+                  alignment: Alignment(0.0, 0.0),
+                  children: <Widget>[
+                    new Container(
+                    height: 300.0,
+                      width: 300.0,
+                      margin: new EdgeInsets.symmetric(
+                          vertical: 2.0,
+                          horizontal: 2.0
+                      ),
+                      child: new Column(
                         children: <Widget>[
-                          new Text(
-                            item.description,
-                            style: new TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white
-                            ),
-                          )
-                        ],),
-                    ],
-                  ),
+                          Expanded(child:Image.network(item.url)),
+                          new Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              new Text(
+                                item.description,
+                                style: new TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white
+                                ),
+                              )
+                            ],),
+                        ],
+                      ),
+                    ),
+                    new Container(
+                      child: new Card(
+                        color:Color.fromARGB(150, 255, 255, 255),
+                        child:new IconButton(
+                          icon: Icon(Icons.play_arrow),
+                          onPressed: (){
+                            _playVideo(item);
+                          },
+                          iconSize: 65.0,
+                        ),
+                      )
+                    )
+                  ],
                 );
+
               },
               itemCount: imageUrls.length,
           )
