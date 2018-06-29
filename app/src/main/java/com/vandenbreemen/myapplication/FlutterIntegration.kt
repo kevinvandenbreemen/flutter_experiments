8243ad6fea171712f0af7da4e7c1fd87d26585d8
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-06-29 11:30:02 -0400

    KEV0-0:  Hook up toggle events

--- app/src/main/java/com/vandenbreemen/myapplication/FlutterIntegration.kt
+++ app/src/main/java/com/vandenbreemen/myapplication/FlutterIntegration.kt
@@ -1,12 +1,32 @@
 package com.vandenbreemen.myapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Switch
 
 class FlutterIntegration : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flutter_integration)
+
+        findViewById<Switch>(R.id.flutterToggle).setOnClickListener(View.OnClickListener { v ->
+            val switch = findViewById<Switch>(R.id.flutterToggle)
+            if(switch.isChecked){
+                engageFlutter()
+            }
+            else{
+                disengageFlutter()
+            }
+        })
+    }
+
+    private fun disengageFlutter() {
+        println("Disengage")
+    }
+
+    private fun engageFlutter() {
+        println("Engage")
     }
 }
