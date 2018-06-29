a592e9f84d4895f920cec74cb77e18a6ef9574a6
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-06-29 14:19:34 -0400

    KEV0-0:  Get basic gallery type UI going

--- flutter_module/lib/business_logic.dart
+++ flutter_module/lib/business_logic.dart
@@ -5,11 +5,40 @@ class GalleryChooser extends StatefulWidget {
   _GalleryChooserState createState() => _GalleryChooserState();
 }
 
+class GalleryItem{
+  final String url;
+  final String description;
+
+  GalleryItem(this.url, this.description);
+
+}
+
+
+
 class _GalleryChooserState extends State<GalleryChooser> {
 
-  List<String> imageUrls = new List<String>();
+  List<GalleryItem> imageUrls = new List<GalleryItem>();
 
+  @override
+  void initState() {
+    imageUrls.add(GalleryItem(
+      "https://images.pexels.com/photos/917476/pexels-photo-917476.jpeg",
+      "Typewriter"
+    ));
+    imageUrls.add(GalleryItem(
+        "https://images.pexels.com/photos/730896/pexels-photo-730896.jpeg",
+        "Cat"
+    ));
+    imageUrls.add(GalleryItem(
+        "https://images.pexels.com/photos/1024994/pexels-photo-1024994.jpeg",
+        "Sunset"
+    ));
 
+  }
+
+  void onSelectUrl(String url){
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +46,29 @@ class _GalleryChooserState extends State<GalleryChooser> {
       padding: new EdgeInsets.all(32.0),
       child: new Column(
         children: <Widget>[
-          new Text("Child List Goeth Here")
+          new Text("Child List Goeth Here"),
+          new Expanded(
+              child: new ListView.builder(
+              itemBuilder: (context, index){
+                GalleryItem item = imageUrls[index];
+                return new Container(
+                  height: 120.0,
+                  margin: new EdgeInsets.symmetric(
+                    vertical: 16.0,
+                    horizontal: 24.0
+                  ),
+                  child: new Column(
+                    children: <Widget>[
+                      new Text(item.description),
+                      Expanded(child:Image.network(item.url))
+                    ],
+                  ),
+                );
+              },
+              itemCount: imageUrls.length,
+          )
+          )
+
         ],
       )
     );
